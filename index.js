f252ddb59371ae6abaf11ab0b2763b5df6a451c5
Marnie K <marnie.kelly.123@gmail.com>
2021-11-28 20:00:02 -0500

    passing salary test

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+let TotalSalary = (lineup) => {
+  return lineup.reduce((salary, player) => {
+    return salary + player.salary
+  }, 0)
+}
+
+let salaryOver = (lineup) => {
+  return TotalSalary(lineup) > 45000
+}
+
 let validateLineup = (lineup) => {
+  return !salaryOver(lineup)
+
 
 }
 
@@ -11,27 +23,34 @@ let validateLineup = (lineup) => {
 2. What are the inputs?
     An array - lineup[]
     consisting of objects with the keys:
-      id:
-      name:
-      position:
-      teamId:
-      gameId:
-      salary:
+      id: string
+      name: string
+      position: string
+      teamId: number
+      gameId: number
+      salary: number
 
 3. What is the output?
       Boolean - returns true when the lineup satisfies all conditions
 
-
 ## Lineup Rules
 1) The total salary of all players in a lineup may not exceed $45,000
       returns false when the lineup has a total salary greater than 45000
-
+        total lineup[i].salary
+        if total > 45000
+        return false
 
 2) Lineups may not contain more than 2 players from a single team
       returns false when the lineup includes too many players from a single team
+      filter to new array based on teamId
+      if length of new array > 2
+      return false
 
 3) Lineups may not contain more than 3 players from a single game
       returns false when the lineup includes too many players from a single game
+      filter to new array based on gameId
+      if length of new array > 2
+      return false
 
 4) Lineups must contain exactly 3 players with the position of 'OF' 
 and must also contain exactly 1 player from each of the following positions: 
